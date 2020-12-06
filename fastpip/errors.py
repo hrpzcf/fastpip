a28383d3e7586bb6e150b53a4424065e9c58fa14
hrpzcf <hrpzcf@foxmail.com>
2020-12-06 15:46:00 +0800

    feat: errors.py新增PyEnvNotFound异常类

--- fastpip/errors.py
+++ fastpip/errors.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-'''异常集合，主要是为了中文异常名。'''
-
 
 class 目录查找异常(Exception):
     pass
@@ -23,4 +21,16 @@ class 参数值异常(Exception):
     pass
 
 
-__all__ = ['目录查找异常', '文件查找异常', '数据类型异常', '适用平台异常', '参数值异常']
+class PyEnvNotFound(Exception):
+    pass
+
+
+__all__ = [
+    '目录查找异常',
+    '文件查找异常',
+    '数据类型异常',
+    '适用平台异常',
+    '参数值异常',
+    'PyEnvNotFound',
+]
+
