a08830943124ae2cf97ae70757df99b62744a561
hrpzcf <hrpzcf@foxmail.com>
2021-02-07 23:45:35 +0800

    release: 0.4.0

--- fastpip/__init__.py
+++ fastpip/__init__.py
@@ -46,7 +46,7 @@ __all__ = [
 ]
 
 NAME = 'fastpip'
-VERSION = '0.3.4'
+VERSION = '0.4.0'
 AUTHOR = 'hrpzcf'
 EMAIL = 'hrpzcf@foxmail.com'
 WEBSITE = 'https://gitee.com/hrpzcf/fastpip'
--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.7',
     ],
     keywords=['pip', 'package'],
 )
