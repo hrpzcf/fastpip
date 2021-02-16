7de1c54a3c1f3cc1df179cba87ebde5620ad4666
hrpzcf <hrpzcf@foxmail.com>
2021-02-16 21:48:31 +0800

    refactor: 版本信息等移至versions.py

--- fastpip/__init__.py
+++ fastpip/__init__.py
@@ -24,13 +24,7 @@ SOFTWARE.'''
 
 from .errors import *
 from .fastpip import PyEnv, all_py_paths, cur_py_path, index_urls
-
-NAME = 'fastpip'
-version = 0, 6, 0
-AUTHOR = 'hrpzcf'
-EMAIL = 'hrpzcf@foxmail.com'
-WEBSITE = 'https://gitee.com/hrpzcf/fastpip'
-VERSION = '.'.join(map(str, version))
+from .versions import *
 
 __all__ = [
     'all_py_paths',
---
+++ fastpip/versions.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+NAME = 'fastpip'
+version = 0, 6, 0
+AUTHOR = 'hrpzcf'
+EMAIL = 'hrpzcf@foxmail.com'
+WEBSITE = 'https://gitee.com/hrpzcf/fastpip'
+VERSION = '.'.join(map(str, version))
+
+__all__ = [
+    'NAME',
+    'AUTHOR',
+    'EMAIL',
+    'WEBSITE',
+    'VERSION',
+    'version',
+]
