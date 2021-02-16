8f6f17e4936e625ade8c80aa65521bed6ebde249
hrpzcf <hrpzcf@foxmail.com>
2021-02-16 22:00:17 +0800

    release: 0.6.1

--- fastpip/versions.py
+++ fastpip/versions.py
@@ -1,17 +1,10 @@
 # coding: utf-8
 
 NAME = 'fastpip'
-version = 0, 6, 0
+version = 0, 6, 1
 AUTHOR = 'hrpzcf'
 EMAIL = 'hrpzcf@foxmail.com'
 WEBSITE = 'https://gitee.com/hrpzcf/fastpip'
 VERSION = '.'.join(map(str, version))
 
-__all__ = [
-    'NAME',
-    'AUTHOR',
-    'EMAIL',
-    'WEBSITE',
-    'VERSION',
-    'version',
-]
+__all__ = ['NAME', 'AUTHOR', 'EMAIL', 'WEBSITE', 'VERSION', 'version']
