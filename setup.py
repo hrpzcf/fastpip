f56267ec2bbc37067a091fd64f39edd20312d2fd
hrpzcf <hrpzcf@foxmail.com>
2021-04-18 22:53:30 +0800

    chore: 更新setup.py文件

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 # coding: utf-8
 
 from setuptools import find_packages, setup
+
 from fastpip import AUTHOR, EMAIL, NAME, VERSION, WEBSITE
 
-description = '一个pip命令包，可在Python3源代码中实现方便的包管理操作。'
-
+description = "一个pip命令包，可在Python3.7+源代码中实现方便的包管理操作。"
 try:
-    with open('README.md', 'r', encoding='utf-8') as mdfile:
+    with open("README.md", "r", encoding="utf-8") as mdfile:
         long_description = mdfile.read()
 except Exception:
     long_description = description
@@ -21,16 +21,19 @@ setup(
     url=WEBSITE,
     description=description,
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    license='MIT License',
+    long_description_content_type="text/markdown",
+    license="MIT License",
     packages=find_packages(),
-    platforms=['win32', 'win_amd64'],
-    install_requires=['psutil>=5.7.2'],
-    python_requires='>=3.7',
+    platforms=["win32", "win_amd64"],
+    install_requires=["psutil>=5.7.2"],
+    python_requires=">=3.7",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.7',
+        "Intended Audience :: Developers",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
-    keywords=['pip', 'package'],
+    keywords=["pip", "package"],
 )
