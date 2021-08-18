a635c6e69c2ceda8120e8137f3a15e0502466a16
hrpzcf <hrpzcf@foxmail.com>
2021-08-18 22:54:09 +0800

    refactor: versions.py文件version变量重命名为VERNUM

--- fastpip/versions.py
+++ fastpip/versions.py
@@ -1,10 +1,10 @@
 # coding: utf-8
 
 NAME = "fastpip"
-version = 0, 11, 5
+VERNUM = 0, 11, 5
 AUTHOR = "hrpzcf"
 EMAIL = "hrpzcf@foxmail.com"
 WEBSITE = "https://gitee.com/hrpzcf/fastpip"
-VERSION = ".".join(map(str, version))
+VERSION = ".".join(map(str, VERNUM))
 
-__all__ = ["NAME", "AUTHOR", "EMAIL", "WEBSITE", "VERSION", "version"]
+__all__ = ["NAME", "AUTHOR", "EMAIL", "WEBSITE", "VERSION", "VERNUM"]
