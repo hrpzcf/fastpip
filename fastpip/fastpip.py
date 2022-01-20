1726222d0fbc3b6138a059f9ac5745172829e2c5
hrpzcf <hrpzcf@foxmail.com>
2022-01-20 14:06:25 +0800

    chore: 修改弃用警告文案

--- fastpip/fastpip.py
+++ fastpip/fastpip.py
@@ -1,13 +1,16 @@
 # coding: utf-8
 
+# TODO 在正式版时清理并删除此模块
+
 from warnings import warn
 
 warn(
-    "警告：fastpip正式版将删除此导入路径，现在可以直接从fastpip导入"
-    "parse_package_names而无需通过模块的相对路径，请尽快更改你的程序的相关代码。",
+    "\n弃用警告：正式版 fastpip 将考虑删除 "
+    "from fastpip.fastpip import parse_package_names 导入路径。"
+    "\n\t新导入方式：from fastpip import parse_package_names 或 from fastpip import *"
+    "\n请考虑将您代码中的旧导入方式改为以上展示的新方式，从这个版本到正式版之间的版本，两种导入方式将同时存在。",
     stacklevel=2,
 )
-
-# 为了兼容改变fastpip目录结构前通过相对路径导入parse_package_names的程序
+# 为了兼容使用旧方法导入的程序
 from .core.fastpip import parse_package_names
 from .utils.findpath import all_py_paths
