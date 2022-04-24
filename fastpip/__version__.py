056e0364bc03239afef6aaf7ed83a3f8cf91b291
hrpzcf <hrpzcf@foxmail.com>
2022-04-24 22:50:15 +0800

    版本 0.13.1。

--- fastpip/__version__.py
+++ fastpip/__version__.py
@@ -3,7 +3,7 @@
 __all__ = ["NAME", "AUTHOR", "EMAIL", "WEBSITE", "VERSION", "VERNUM"]
 
 NAME = "fastpip"
-VERNUM = 0, 13, 0
+VERNUM = 0, 13, 1
 AUTHOR = "hrpzcf"
 EMAIL = "hrpzcf@foxmail.com"
 WEBSITE = "https://github.com/hrpzcf/fastpip"
