1d98abebfaf28daa2379060f2db816ffc41594f0
hrpzcf <pphrp@qq.com>
2022-10-13 02:15:06 +0800

    更新 setup.py 文件

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 # coding: utf-8
 
 from glob import glob
+
 from setuptools import find_packages, setup
 
 from fastpip import AUTHOR, EMAIL, NAME, VERSION, WEBSITE
 
-description = "一个 pip 命令包，可实现用 Python 代码进行方便的包管理操作。"
+description = "一个 pip 命令包，帮助你实现使用 Python 编程的方式进行包管理操作。"
 try:
-    with open("README.md", "r", encoding="utf-8") as mdfile:
+    with open("README.md", "rt", encoding="utf-8") as mdfile:
         long_description = mdfile.read()
 except Exception:
     long_description = description
@@ -39,6 +40,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     keywords=["pip", "package"],
 )
