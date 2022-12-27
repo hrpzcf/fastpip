448f3d71f480dac8a171df7ddac06ef78f6c5971
hrpzcf <pphrp@qq.com>
2022-12-28 02:49:35 +0800

    版本：1.6.0

--- fastpip/__version__.py
+++ fastpip/__version__.py
@@ -3,7 +3,7 @@
 __all__ = ["NAME", "AUTHOR", "WEBSITE", "VERSION", "VERNUM"]
 
 NAME = "fastpip"
-VERNUM = 1, 5, 1
+VERNUM = 1, 6, 0
 AUTHOR = "hrpzcf"
 WEBSITE = "https://github.com/hrpzcf/fastpip"
 VERSION = ".".join(map(str, VERNUM))
